583829e8b73b037b65df6043fd07146b2b81370e
Dmitriy <dmdperm@yandex.ru>
2024-04-15 02:03:13 +0500

    editing

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
 
     var amount = 50;
-    var generalCommission = amount * 0.75
+    var generalCommission = amount * 0.075
     var commission = if(generalCommission>35) generalCommission else 35;
 
     println(commission);
